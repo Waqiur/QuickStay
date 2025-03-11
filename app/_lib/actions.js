cc42d215264ddbe587dbe983299a7d4b0fd36b7d
Waqiur <Wakiurrahmanansari@gmail.com>
2025-03-11 19:50:17 +0530

    Enhance reservation functionality with date selection, booking creation, and loading states

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -29,6 +29,30 @@ export async function updateGuest(formData) {
     }
     revalidatePath("/account/profile");
 }
+export async function createBooking(bookingData, formData) {
+    const session = await auth();
+    if (!session) throw new Error("You must be logged in");
+
+    const newBooking = {
+        ...bookingData,
+        guestID: session.user.guestId,
+        numGuests: Number(formData.get("numGuests")),
+        observations: formData.get("observations").slice(0, 1000),
+        extraPrice: 0,
+        totalPrice: bookingData.cabinPrice,
+        isPaid: false,
+        hasBreakfast: false,
+        status: "unconfirmed",
+    };
+    console.log(newBooking);
+    const { error } = await supabase.from("bookings").insert([newBooking]);
+
+    if (error) throw new Error("Booking could not be created");
+
+    revalidatePath(`/cabins/${bookingData.cabinID}`);
+
+    redirect("/cabins/thankyou");
+}
 export async function deleteBooking(bookingId) {
     const session = await auth();
     if (!session) throw new Error("You must be logged in");
